bf818c19d186ddb4c04061bd7b84b907f503f1d8
Manav <manavjangid5@gmail.com>
2023-04-30 22:29:06 +0530

    delete and login

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash,session, redirect
+from flask_session import Session
 from flask_mysqldb import MySQL
 from werkzeug.utils import secure_filename
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators, DateTimeField, BooleanField, IntegerField, DecimalField, HiddenField, SelectField, RadioField
 from flask_wtf import FlaskForm
+from functools import wraps
 from wtforms.fields import DateField, TimeField
 from datetime import timedelta, datetime
 from wtforms.validators import ValidationError, NumberRange,InputRequired,Length
@@ -22,6 +24,20 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 app.secret_key = 'cvproject'
 mysql = MySQL(app)
+
+def user_role_professor(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            if session['user_role'] == "teacher":
+                return f(*args, **kwargs)
+            else:
+                flash('You dont have privilege to access this page!','danger')
+                return render_template("404.html")
+        else:
+            flash('Unauthorized, Please login!','danger')
+            return redirect(url_for('login'))
+    return wrap
 
 class UploadForm(FlaskForm):
 	subject = StringField('Subject',validators=[InputRequired(message="required")])
@@ -52,7 +68,52 @@ class UploadForm(FlaskForm):
 
 @app.route("/")
 def index():
-	return render_template('index.html', messages = 'My name is Khan')
+	return render_template('index.html', messages = 'My name is proctor')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+	if request.method == 'POST':
+		email = request.form['email']
+		password_candidate = request.form['password']
+		user_type = request.form['user_type']
+		imgdata1 = request.form['image_hidden']
+		cur = mysql.connection.cursor()
+		results1 = cur.execute('SELECT uid, name, email, password, user_type, user_image from users where email = %s and user_type = %s and user_login = 0' , (email,user_type))
+		if results1 > 0:
+			cresults = cur.fetchone()
+			imgdata2 = cresults['user_image']
+			password = cresults['password']
+			name = cresults['name']
+			uid = cresults['uid']
+			nparr1 = np.frombuffer(base64.b64decode(imgdata1), np.uint8)
+			nparr2 = np.frombuffer(base64.b64decode(imgdata2), np.uint8)
+			image1 = cv2.imdecode(nparr1, cv2.COLOR_BGR2GRAY)
+			image2 = cv2.imdecode(nparr2, cv2.COLOR_BGR2GRAY)
+			img_result  = DeepFace.verify(image1, image2, enforce_detection = False)
+			if img_result["verified"] == True and password == password_candidate:
+				results2 = cur.execute('UPDATE users set user_login = 1 where email = %s' , [email])
+				mysql.connection.commit()
+				if results2 > 0:
+					session['logged_in'] = True
+					session['email'] = email
+					session['name'] = name
+					session['user_role'] = user_type
+					session['uid'] = uid
+					if user_type == "student":
+						return redirect(url_for('student_index'))
+					else:
+						return redirect(url_for('professor_index'))
+				else:
+					error = 'Error Occurred!'
+					return render_template('login.html', error=error)	
+			else:
+				error = 'Either Image not Verified or you have entered Invalid password or Already login'
+				return render_template('login.html', error=error)
+			cur.close()
+		else:
+			error = 'Already Login or Email was not found!'
+			return render_template('login.html', error=error)
+	return render_template('login.html')
 
 @app.route("/professor_index")
 def professor_index():
@@ -114,5 +175,51 @@ def test_generate():
 		else:
 			return None
 
+@app.route('/deltidlist', methods=['GET'])
+@user_role_professor
+def deltidlist():
+	cur = mysql.connection.cursor()
+	results = cur.execute('SELECT * from teachers where email = %s and uid = %s', (session['email'], session['uid']))
+	if results > 0:
+		cresults = cur.fetchall()
+		now = datetime.now()
+		now = now.strftime("%Y-%m-%d %H:%M:%S")
+		now = datetime.strptime(now,"%Y-%m-%d %H:%M:%S")
+		testids = []
+		for a in cresults:
+			if datetime.strptime(str(a['start']),"%Y-%m-%d %H:%M:%S") > now:
+				testids.append(a['test_id'])
+		cur.close()
+		return render_template("deltidlist.html", cresults = testids)
+	else:
+		return render_template("deltidlist.html", cresults = None)
+
+@app.route('/deldispques', methods=['GET','POST'])
+@user_role_professor
+def deldispques():
+	if request.method == 'POST':
+		tidoption = request.form['choosetid']
+		et = examtypecheck(tidoption)
+		if et['test_type'] == "objective":
+			cur = mysql.connection.cursor()
+			cur.execute('SELECT * from questions where test_id = %s and uid = %s', (tidoption,session['uid']))
+			callresults = cur.fetchall()
+			cur.close()
+			return render_template("deldispques.html", callresults = callresults, tid = tidoption)
+		elif et['test_type'] == "subjective":
+			cur = mysql.connection.cursor()
+			cur.execute('SELECT * from longqa where test_id = %s and uid = %s', (tidoption,session['uid']))
+			callresults = cur.fetchall()
+			cur.close()
+			return render_template("deldispquesLQA.html", callresults = callresults, tid = tidoption)
+		elif et['test_type'] == "practical":
+			cur = mysql.connection.cursor()
+			cur.execute('SELECT * from practicalqa where test_id = %s and uid = %s', (tidoption,session['uid']))
+			callresults = cur.fetchall()
+			cur.close()
+			return render_template("deldispquesPQA.html", callresults = callresults, tid = tidoption)
+		else:
+			flash("Some Error Occured!")
+			return redirect(url_for('/deltidlist'))
 if __name__ == "__main__":
 	app.run()
